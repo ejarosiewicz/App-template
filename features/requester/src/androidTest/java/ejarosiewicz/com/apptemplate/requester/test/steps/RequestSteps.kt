5309b4fca481d6c288b2065f61e55d883189b41b
Emil Jarosiewicz <emiljarosiewicz@gmail.com>
2019-09-04 21:07:56 +0200

    Add robot for automated test

--- features/requester/src/androidTest/java/ejarosiewicz/com/apptemplate/requester/test/steps/RequestSteps.kt
+++ features/requester/src/androidTest/java/ejarosiewicz/com/apptemplate/requester/test/steps/RequestSteps.kt
@@ -1,24 +1,25 @@
 package ejarosiewicz.com.apptemplate.requester.test.steps
 
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.mauriciotogneri.greencoffee.GreenCoffeeSteps
 import com.mauriciotogneri.greencoffee.annotations.Then
 import com.mauriciotogneri.greencoffee.annotations.When
+import ejarosiewicz.com.apptemplate.requester.test.robots.basic
 import ejarosiewicz.com.requester.R
-
 
 class RequestSteps: GreenCoffeeSteps() {
 
     @When("^I want to make request$")
     fun makeSomeRequest() {
-        onViewWithId(R.id.textView).click()
+        basic {
+            click(R.id.textView)
+        }
     }
 
     @Then("^I see the result on the screen$")
     fun verifyResults() {
-        onViewWithId(R.id.textView).check(matches(withText("chuj")))
+        basic {
+            matchText(R.id.textView, "world")
+        }
     }
-
-
 }
+
