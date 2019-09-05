5dbdb5368eb4e277463428f57064b8cf857ea72d
Emil Jarosiewicz <emiljarosiewicz@gmail.com>
2019-09-05 20:47:26 +0200

    Move tests into app package

--- app/src/androidTest/java/ejarosiewicz/com/apptemplate/requester/test/SimpleRequestFeatureTest.kt
+++ app/src/androidTest/java/ejarosiewicz/com/apptemplate/requester/test/SimpleRequestFeatureTest.kt
@@ -1,12 +1,9 @@
 package ejarosiewicz.com.apptemplate.requester.test
 
-import android.app.Activity
 import androidx.test.rule.ActivityTestRule
 import com.mauriciotogneri.greencoffee.GreenCoffeeConfig
 import com.mauriciotogneri.greencoffee.GreenCoffeeTest
 import com.mauriciotogneri.greencoffee.ScenarioConfig
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
 import ejarosiewicz.com.apptemplate.requester.test.steps.RequestSteps
 import ejarosiewicz.com.apptemplate.requester.view.RequesterActivity
 import org.junit.Rule
@@ -14,9 +11,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.io.IOException
-import dagger.android.AndroidInjector.Factory
-import dagger.android.DispatchingAndroidInjector_Factory
-import javax.inject.Provider
 
 
 @RunWith(Parameterized::class)
@@ -43,17 +37,4 @@ class SimpleRequestFeatureTest(scenarioConfig: ScenarioConfig) : GreenCoffeeTest
     fun test() {
         start(RequestSteps())
     }
-}
-
-
-inline fun <reified T : Activity> createFakeActivityInjector(crossinline block : T.() -> Unit)
-        : DispatchingAndroidInjector<Any> {
-    val injector = AndroidInjector<Activity> { instance ->
-        if (instance is T) {
-            instance.block()
-        }
-    }
-    val factory = Factory<Activity> { injector }
-    val map = mapOf(Pair<Class <*>, Provider<Factory<*>>>(T::class.java, Provider { factory }))
-    return DispatchingAndroidInjector_Factory.newInstance(map, emptyMap())
 }
