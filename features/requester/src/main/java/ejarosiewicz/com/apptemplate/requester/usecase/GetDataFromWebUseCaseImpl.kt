5d43afb587ebfbe6c79ce1ac33c8e0f6f4155780
Emil Jarosiewicz <emiljarosiewicz@gmail.com>
2019-09-25 20:47:21 +0200

    Add header at the top

--- features/requester/src/main/java/ejarosiewicz/com/apptemplate/requester/usecase/GetDataFromWebUseCaseImpl.kt
+++ features/requester/src/main/java/ejarosiewicz/com/apptemplate/requester/usecase/GetDataFromWebUseCaseImpl.kt
@@ -2,7 +2,9 @@ package ejarosiewicz.com.apptemplate.requester.usecase
 
 import ejarosiewicz.com.apptemplate.requester.usecase.converter.SomeDataToDataToShowConverter
 import ejarosiewicz.com.apptemplate.requester.usecase.data.DataToShow
+import ejarosiewicz.com.apptemplate.requester.usecase.data.Header
 import ejarosiewicz.com.network.RequestProvider
+import ejarosiewicz.com.network.data.SomeData
 import io.reactivex.Single
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -15,7 +17,9 @@ class GetDataFromWebUseCaseImpl @Inject constructor(
     GetDataFromWebUseCase {
 
     override fun load(): Single<List<DataToShow>> = requestProvider.someRequest()
-        .map { someDataList ->
-            someDataList.map { data -> converter.convert(data) }
-        }
+        .map { someDataList -> someDataList.prepareDataList() }
+
+    private fun List<SomeData>.prepareDataList() = this
+        .map { data -> converter.convert(data) }
+        .sortedBy { dataToShow -> dataToShow !is Header }
 }
--- features/requester/src/test/java/ejarosiewicz/com/apptemplate/requester/usecase/GetDataFromWebUseCaseImplTest.kt
+++ features/requester/src/test/java/ejarosiewicz/com/apptemplate/requester/usecase/GetDataFromWebUseCaseImplTest.kt
@@ -3,8 +3,10 @@ package ejarosiewicz.com.apptemplate.requester.usecase
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import ejarosiewicz.com.apptemplate.requester.usecase.converter.SomeDataToDataToShowConverter
 import ejarosiewicz.com.apptemplate.requester.usecase.data.DataToShow
+import ejarosiewicz.com.apptemplate.requester.usecase.data.Header
 import ejarosiewicz.com.network.RequestProvider
 import ejarosiewicz.com.network.data.SomeData
 import io.reactivex.Single
@@ -18,6 +20,7 @@ class GetDataFromWebUseCaseImplTest {
     }
     private val mockConverter: SomeDataToDataToShowConverter = mock {
         on { convert(STUB_SOME_DATA) } doReturn mockDataToShow
+        on { convert(STUB_SOME_DATA_HEADER) } doReturn STUB_HEADER
     }
 
     private val systemUnderTest = GetDataFromWebUseCaseImpl(mockRequestProvider, mockConverter)
@@ -37,7 +40,27 @@ class GetDataFromWebUseCaseImplTest {
         verify(mockConverter).convert(STUB_SOME_DATA)
     }
 
+    @Test
+    fun `Add header at the beginning`() {
+        givenListWithLastHeader()
+
+        systemUnderTest.load()
+            .test()
+            .assertOf { observer ->
+                observer.assertValue { list ->
+                    list[0] is Header
+                }
+            }
+    }
+
+    private fun givenListWithLastHeader(){
+        val listWithHeaderLast = listOf(STUB_SOME_DATA, STUB_SOME_DATA, STUB_SOME_DATA_HEADER)
+        whenever(mockRequestProvider.someRequest()).thenReturn(Single.just(listWithHeaderLast))
+    }
+
     companion object {
         private val STUB_SOME_DATA = SomeData(false, "yellow", "http://hotporn.com/ass.png")
+        private val STUB_SOME_DATA_HEADER = SomeData(true, "header", "http://hotporn.com/ass.png")
+        private val STUB_HEADER = Header("head", "enough")
     }
 }
